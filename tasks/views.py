f9ec3fcb77b69e0c064bb1c5a90e0bad5a91ecf2
EstebanGC <johan.gal.car@gmail.com>
2023-01-24 19:43:35 -0500

    Initial commit
    Signup and Tasks templates added

---
+++ tasks/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Username already exists'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Passwords do not match'
+        })
+
+
+def tasks(request): 
+    return render(request, 'tasks.html' )
